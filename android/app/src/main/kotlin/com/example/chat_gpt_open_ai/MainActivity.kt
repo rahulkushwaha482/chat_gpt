0514f0032e4f65af2202641a6510872205ef77b2
Rahul Kushwaha <rahulkushwaha@Rahuls-Mac-mini.local>
2023-01-13 17:52:08 +0530

    chat gpt api integrated in flutter

---
+++ android/app/src/main/kotlin/com/example/chat_gpt_open_ai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_gpt_open_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constatnt.dart
@@ -0,0 +1,2 @@
+
+const apiSecretKey = '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';
